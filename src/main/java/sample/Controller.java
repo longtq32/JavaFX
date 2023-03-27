195921d2ac90d4135eacb16d8f37b25cda07a428
Tu Long Latitude <longtq32@gmail.com>
2023-03-28 00:06:02 +0700

    delete any code in Controller.java

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -7,17 +7,6 @@ import javafx.scene.control.Label;
 public class Controller {
 
     @FXML
-    private Label shortText;
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-        shortText.setVisible(false);
-    }
-
-    @FXML
     public void showAddContactDialog() {
     }
 }
