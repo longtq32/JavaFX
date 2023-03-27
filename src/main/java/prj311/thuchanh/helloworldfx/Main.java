0104fc24ad45b0b33bd04ca9eb8089e521abff79
Tu Long Latitude <longtq32@gmail.com>
2023-03-27 12:21:51 +0700

    modifiel name Main.java

--- src/main/java/prj311/thuchanh/helloworldfx/Main.java
+++ src/main/java/prj311/thuchanh/helloworldfx/Main.java
@@ -4,19 +4,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.lang.reflect.Parameter;
 import java.util.List;
 import java.util.Map;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
 
 //        Text msg = new Text("Hello JavaFX ok");
@@ -67,5 +64,7 @@ public class HelloApplication extends Application {
     // dung git lan 5 edit in destop home
     // dung git lan 6 edit in desktop home
     // dung git Lan 8 edit in desktop office
+
+
 
 }
